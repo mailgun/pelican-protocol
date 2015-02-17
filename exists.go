c90d6535229f41aaa3243a27e875e52055b31a76
vagrant <jaten@causalnetworks.com>
2015-02-17 04:22:57 +0000

    more docs

--- exists.go
+++ exists.go
@@ -4,6 +4,7 @@ import (
 	"os"
 )
 
+// FileExists returns true iff the path name is a file (and not a directory or non-existant).
 func FileExists(name string) bool {
 	fi, err := os.Stat(name)
 	if err != nil {
@@ -15,6 +16,8 @@ func FileExists(name string) bool {
 	return true
 }
 
+// FileExistsLen check if name is an actually file (directories don't count) and also
+// returns the length of the file.
 func FileExistsLen(name string) (bool, int64) {
 	fi, err := os.Stat(name)
 	if err != nil {
@@ -26,6 +29,7 @@ func FileExistsLen(name string) (bool, int64) {
 	return true, fi.Size()
 }
 
+// DirExists returns true if name represents a directory on disk.
 func DirExists(name string) bool {
 	fi, err := os.Stat(name)
 	if err != nil {
