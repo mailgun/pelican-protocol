2ccf58bf5c7194ce529092d208415b6790c22a43
vagrant <jaten@causalnetworks.com>
2015-02-17 04:51:37 +0000

    docs++

--- verbose.go
+++ verbose.go
@@ -5,19 +5,23 @@ import (
 	"time"
 )
 
+// Verbose can be set to true for debug output. For production builds it
+// should be set to false, the default.
 var Verbose bool
 
-// get timestamp for logging purposes
-func ts() string {
+// Ts gets the current timestamp for logging purposes.
+func Ts() string {
 	return time.Now().Format("2006-01-02 15:04:05.999 -0700 MST")
 }
 
-// time-stamped printf
+// time-stamped fmt.Printf
 func TSPrintf(format string, a ...interface{}) {
-	fmt.Printf("%s ", ts())
+	fmt.Printf("%s ", Ts())
 	fmt.Printf(format, a...)
 }
 
+// VPrintf is like fmt.Printf, but only prints if Verbose is true. Uses TSPrint
+// to mark each print with a timestamp.
 func VPrintf(format string, a ...interface{}) {
 	if Verbose {
 		TSPrintf(format, a...)
