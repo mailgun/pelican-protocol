f48b8546ecc01bb716283526dbc890cb3b1a0575
Jason E. Aten <j.e.aten@gmail.com>
2015-02-25 01:39:13 -0800

    additional notes

--- backup_test.go
+++ backup_test.go
@@ -51,5 +51,74 @@ simple 1-1 client-server much more appealing. So the server can send you your ke
 
 Before the login/ "reset password" is ever encountered, download the keys from skynet/blockchain/requesting that mailgun send them back to you in an email. Can you make this request over an already secured channel?
 
+"""TrueCrypt. does 3 layer:
+AES-Twofish-Serpent and you can do them in just about any combination. And,
+ you can use RIPEMD or SHA as a hashing algorithm."""
+
+
+-----------------
+backups... tied to a particular host/ip, yes? what if that ip changes? tied to a particular device?
+then it becomes identifiable with that computer, no?/ identifies the computer.
+
+If you don't give the MITM notice that the connection is going to be ssh-http, then he can't modify it to downgrade it. And there are a ton of 'http://' strings that don't want to be changed in web pages for back-compat reasons.
+
+** feature todo: once-upgraded from HTTP->SSH, we should give a warning on downgrade back to HTTP at the same site
+.
+
+problem: traffic monitoring at gateways that won't let ssh traffic tunnel over http.
+for proxy to work, we actually need to use TLS to connect for everything, otherwise firewalls will filter port 80 traffic that is not http. Or: can we tunnel
+https://github.com/nf/gohttptun/ <- a tunnel over http by Andrew Gerrand.
+
+http://stackoverflow.com/questions/21417223/simple-ssh-port-forward-in-golang
+
+
+look at what ssh -D does: b/c it sets up a socks proxy.
+
+
+goproxy for all requests (both http and https):
+  -> https can get through okay, and we could tunnel ssh over https -> then back to the https (localhost :443) port.
+  -> http seems to require additional work to tunnel ssh, like using Andrew Gerrand's ng/gohttuptun.
+
+
+browser side:
+https://github.com/nwjs/nw.js  (chromium + nodejs)
+and
+https://github.com/lonnc/golang-nw
+
+examples (write your own browser solution)
+https://github.com/nwjs/nw.js/wiki/List-of-apps-and-companies-using-nw.js
+
+
+http://shadowsocks.org/en/index.html
+A secure socks5 proxy
+
+https://github.com/nihgwu/Nevermore <- wraps shadowsocks with a nw.js UI
+
+https://github.com/CzarekTomczak/cef2go
+
+-------------
+start by describing the ideal.
+--
+then describe the barrier to adoption, and give the workaround.
+
+https://github.com/inconshreveable/go-tunnel  ### doesn't actually do what we want.
+
+http://http-tunnel.sourceforge.net/
+
+http://sshh.sourceforge.net/
+
+
+////////////////////
+// bosh
+
+http://en.wikipedia.org/wiki/HTTP_tunnel#HTTP_CONNECT_tunneling
+http://en.wikipedia.org/wiki/BOSH
+http://xmpp.org/about-xmpp/technology-overview/bosh/
+
+http://xmpp.org/extensions/xep-0124.html
+
+aka long polling
+
+
 
 */
