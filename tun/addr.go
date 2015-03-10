d48e8178cbd4cbae717fa5bb6eb12ee338ca88dd
vagrant <jaten@causalnetworks.com>
2015-03-10 22:33:02 +0000

    fix NewAddr1 ipport parsing

--- tun/addr.go
+++ tun/addr.go
@@ -1,6 +1,10 @@
 package pelicantun
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 type addr struct {
 	Ip     string
@@ -20,14 +24,24 @@ func (a *addr) SetIpPort() {
 	a.IpPort = fmt.Sprintf("%s:%d", a.Ip, a.Port)
 }
 
+var ipColonPortRegex = regexp.MustCompile(`^([^:]*)\:(.*)$`)
+
 func NewAddr1(ipport string) addr {
 	var ip string
 	var port int
+	var err error
 
-	n, err := fmt.Scanf("%s:%d", &ip, &port)
-	if n != 2 || err != nil {
-		panic(fmt.Sprintf("bad scanf of ipport input '%s': got %d scanned and err: '%s'", ipport, n, err))
+	match := ipColonPortRegex.FindStringSubmatch(ipport)
+	//fmt.Printf("match = %#v\n  for ipport='%s'\n", match, ipport)
+	if match == nil || len(match) != 3 {
+		panic(fmt.Sprintf("bad scanf of ipport input '%s': could not find ip:port match", ipport))
 	}
+	ip = match[1]
+	port, err = strconv.Atoi(match[2])
+	panicOn(err)
+
+	//fmt.Printf("ip = '%s', port = %d\n", ip, port)
+
 	return addr{
 		Ip:     ip,
 		Port:   port,
