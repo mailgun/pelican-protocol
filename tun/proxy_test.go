0dca9cc0525647fbd18dd31ac1e2e1ff864a47ae
Jason E. Aten <j.e.aten@gmail.com>
2015-03-10 00:08:41 -0700

    ft for round trip through fwd proxy

--- tun/proxy_test.go
+++ tun/proxy_test.go
@@ -2,53 +2,67 @@ package pelicantun
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 
 	cv "github.com/glycerine/goconvey/convey"
 )
 
-func TestSocksProxyTalksToReverseProxy002(t *testing.T) {
+func TestFullRoundtripSocksProxyTalksToReverseProxy002(t *testing.T) {
 
-	fmt.Printf("\n before NewReverseProxy\n")
-	rev := NewReverseProxy(ReverseProxyConfig{})
-	fmt.Printf("\n done with NewReverseProxy\n")
+	// setup a mock web server that replies to ping with pong.
+	mux := http.NewServeMux()
+
+	// ping allows our test machinery to function
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		r.Body.Close()
+		fmt.Fprintf(w, "pong")
+	})
+
+	web := NewWebServer(WebServerConfig{}, mux)
+	web.Start()
+	defer web.Stop()
+
+	if !PortIsBound(web.Cfg.Listen.IpPort) {
+		panic("web server did not come up")
+	}
+
+	// start a reverse proxy
+	rev := NewReverseProxy(ReverseProxyConfig{Dest: web.Cfg.Listen})
 	rev.Start()
+	defer rev.Stop()
+
+	if !PortIsBound(rev.Cfg.Listen.IpPort) {
+		panic("web server did not come up")
+	}
 
 	fmt.Printf("\n done with rev.Start(), rev.Cfg.Listen.IpPort = '%v'\n", rev.Cfg.Listen.IpPort)
 
-	revListen := rev.Cfg.Listen
 	if !PortIsBound(rev.Cfg.Listen.IpPort) {
 		panic("rev proxy not up")
 	}
 
+	// start the forward proxy, talks to the reverse proxy.
 	fwd := NewPelicanSocksProxy(PelicanSocksProxyConfig{
-		Dest: revListen,
+		Dest: rev.Cfg.Listen,
 	})
+	fwd.Start()
+	if !PortIsBound(fwd.Cfg.Listen.IpPort) {
+		panic("fwd proxy not up")
+	}
+
+	fmt.Printf("proxy_test: fwd proxy chose listen port = '%#v'\n", fwd.Cfg)
+
+	fmt.Printf("\nproxy_test: both fwd and rev started! \n")
 
 	cv.Convey("\n Given a ForwardProxy and a ReverseProxy, they should communicate over http\n\n", t, func() {
 
-		fmt.Printf("\n after fwd := NewPelicanSocksProxy, before calling fwd.Start() \n")
-
-		fwd.Start()
-		fmt.Printf("fwd proxy chose listen port = '%#v'\n", fwd.Cfg)
-
-		fmt.Printf("\n both fwd and rev started! \n")
-		cv.So(PortIsBound(fwd.Cfg.Listen.IpPort), cv.ShouldEqual, true)
-
 		po("\n fetching url from %v\n", fwd.Cfg.Listen.IpPort)
-		by, err := FetchUrl("http://" + fwd.Cfg.Listen.IpPort + "/")
+
+		by, err := FetchUrl("http://" + fwd.Cfg.Listen.IpPort + "/ping")
 		cv.So(err, cv.ShouldEqual, nil)
-		cv.So(by, cv.ShouldResemble, []byte("some output"))
-
-		fwd.Stop()
-		cv.So(PortIsBound(fwd.Cfg.Listen.IpPort), cv.ShouldEqual, false)
-
-		rev.Stop()
-		cv.So(PortIsBound(rev.Cfg.Listen.IpPort), cv.ShouldEqual, false)
-
-		by, err = FetchUrl("http://" + rev.Cfg.Listen.IpPort + "/")
-		cv.So(err, cv.ShouldEqual, nil)
-		cv.So(by, cv.ShouldResemble, []byte("some output"))
+		//fmt.Printf("by:'%s'\n", string(by))
+		cv.So(string(by), cv.ShouldEqual, "pong")
 	})
 
 	fmt.Printf("\n done with TestSocksProxyTalksToReverseProxy002()\n")
