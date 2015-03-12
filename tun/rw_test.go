af1abc042250b34a73d482922c5653e8fbf08182
Jason E. Aten <j.e.aten@gmail.com>
2015-03-12 13:10:28 -0700

    verified the rw passes even with too small buffers

--- tun/rw_test.go
+++ tun/rw_test.go
@@ -3,8 +3,10 @@ package pelicantun
 import (
 	"net"
 	"testing"
+	"time"
 
 	cv "github.com/glycerine/goconvey/convey"
+	"github.com/glycerine/rbuf"
 )
 
 func TestRW017(t *testing.T) {
@@ -28,9 +30,24 @@ func TestRW017(t *testing.T) {
 	m1 := []byte("yippeeee!")
 	upReadToDnWrite <- m1
 
-	m2 := <-dnReadToUpWrite
+	// accumulate reads into here
+	circ := rbuf.NewFixedSizeRingBuf(200)
 
-	if string(m1) != string(m2) {
+for10msec:
+	for {
+		select {
+		case m2 := <-dnReadToUpWrite:
+			circ.Write(m2)
+		case <-time.After(10 * time.Millisecond):
+			break for10msec
+		}
+	}
+
+	s1 := string(m1)
+	m2 := circ.Bytes()
+	s2 := string(m2)
+	if s1 != s2 {
+		po("m1 = '%s' while m2 = '%s'\n", s1, s2)
 		panic("echo server not echoing")
 	}
 
