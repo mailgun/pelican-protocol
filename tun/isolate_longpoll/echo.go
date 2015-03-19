0e995848692dfc0f4cf5abae343bcabb8753d926
vagrant <jaten@causalnetworks.com>
2015-03-19 07:01:34 +0000

    isolate_longpoll test looking good

---
+++ tun/isolate_longpoll/echo.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+// simple echo server for testing
+//
+// ============================================
+
+type EchoServer struct {
+	Listen Addr
+
+	Ready   chan bool
+	reqStop chan bool
+	Done    chan bool
+
+	lsn net.Listener
+
+	FirstClient chan bool
+	mut         sync.Mutex
+	count       bool
+}
+
+func NewEchoServer(a Addr, count bool) *EchoServer {
+	if a.Port == 0 {
+		a.Port = GetAvailPort()
+	}
+	if a.Ip == "" {
+		a.Ip = "0.0.0.0"
+	}
+	a.SetIpPort()
+
+	r := &EchoServer{
+		Listen:      a,
+		Ready:       make(chan bool),
+		reqStop:     make(chan bool),
+		Done:        make(chan bool),
+		FirstClient: make(chan bool),
+		count:       count,
+	}
+	return r
+}
+
+func (r *EchoServer) Start() error {
+
+	var err error
+	r.lsn, err = net.Listen("tcp", r.Listen.IpPort)
+	if err != nil {
+		return err
+	}
+	go func() {
+		// Insure proper close down on all exit paths.
+		defer func() {
+			r.lsn.Close()
+			close(r.Done)
+		}()
+
+		close(r.Ready)
+		echoCount := 0
+
+		// the Accept loop
+		for {
+			//po("EchoServer::Start(): top of for{} loop.\n")
+			if r.IsStopRequested() {
+				return
+			}
+
+			const serverReadTimeoutMsec = 100
+			err := r.lsn.(*net.TCPListener).SetDeadline(time.Now().Add(time.Millisecond * serverReadTimeoutMsec))
+			panicOn(err)
+
+			conn, err := r.lsn.Accept()
+			if err != nil {
+				if r.IsStopRequested() {
+					return
+				}
+
+				if strings.HasSuffix(err.Error(), "i/o timeout") {
+					// okay, ignore
+				} else {
+					panic(fmt.Sprintf("server EchoServer::Start(): error duing listener.Accept(): '%s'\n", err))
+				}
+				continue // accept again
+			}
+
+			// close FirstClient only once
+			select {
+			case <-r.FirstClient:
+			default:
+				close(r.FirstClient)
+			}
+
+			//po("server EchoServer::Start(): accepted '%v' -> '%v' local.\n", conn.RemoteAddr(), conn.LocalAddr())
+
+			// read from the connections to service clients
+
+			go func(netconn net.Conn) {
+				buf := make([]byte, 4096)
+				for {
+					if r.IsStopRequested() {
+						return
+					}
+
+					err = netconn.SetReadDeadline(time.Now().Add(2000 * time.Millisecond))
+					panicOn(err)
+
+					n, _ := netconn.Read(buf)
+
+					if n > 0 {
+						po("echo service routine read buf '%s'\n", string(buf[:n]))
+
+						time.Sleep(1 * time.Second)
+
+						err = netconn.SetWriteDeadline(time.Now().Add(2000 * time.Millisecond))
+						panicOn(err)
+
+						netconn.Write(buf[:n])
+						if r.count {
+							netconn.Write([]byte(fmt.Sprintf("%d", echoCount)))
+						}
+						echoCount++
+						po("echoCount = %d", echoCount)
+					}
+					//nw, _ := netconn.Write(buf[:n])
+					//po("echo service routine wrote buf '%s'\n", string(buf[:nw]))
+				}
+			}(conn)
+
+		}
+
+	}()
+	<-r.Ready
+	WaitUntilServerUp(r.Listen.IpPort)
+	return nil
+}
+
+func (r *EchoServer) Nonecho(msg string) {
+	// tell next to arrive socket msg instead of echoing what they gave us.
+
+	po("\n\n  EchoServer::Nonecho() called with msg = '%s'\n\n", msg)
+
+}
+
+func (r *EchoServer) IsStopRequested() bool {
+	select {
+	case <-r.reqStop:
+		return true
+	default:
+		return false
+	}
+}
+
+func (r *EchoServer) Stop() {
+	r.RequestStop()
+	r.lsn.Close()
+	<-r.Done
+}
+
+// RequestStop makes sure we only close
+// the s.reqStop channel once. Returns
+// true iff we closed s.reqStop on this call.
+func (s *EchoServer) RequestStop() bool {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	select {
+	case <-s.reqStop:
+		return false
+	default:
+		close(s.reqStop)
+		return true
+	}
+}
---
+++ tun/isolate_longpoll/web.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	// _ "net/http/pprof" // side-effect: installs handlers for /debug/pprof
+)
+
+type WebServer struct {
+	ServerReady chan bool // closed once server is listening on Addr
+	Done        chan bool // closed when server shutdown.
+
+	requestStop chan bool // private. Users should call Stop().
+
+	// we use tigertonic-based web-server because it implements graceful stopping;
+	// as opposed to the built-in http library web server.
+	tts *CustomHttpServer
+
+	started bool
+	stopped bool
+	Cfg     WebServerConfig
+
+	Name string // distinguish for debug prints the various web server uses/at Start()
+	mut  sync.Mutex
+}
+
+type WebServerConfig struct {
+	Listen      Addr
+	ReadTimeout time.Duration
+}
+
+func NewWebServer(cfg WebServerConfig, mux *http.ServeMux) (*WebServer, error) {
+
+	if mux == nil {
+		mux = http.NewServeMux()
+	}
+
+	// get an available port
+	if cfg.Listen.Port == 0 {
+		cfg.Listen.Port = GetAvailPort()
+	}
+	if cfg.Listen.Ip == "" {
+		cfg.Listen.Ip = "0.0.0.0"
+	}
+	cfg.Listen.SetIpPort()
+	//VPrintf("hey hey: starting webserver on '%s'\n", cfg.Listen.IpPort)
+
+	// check that it isn't already occupied
+	if PortIsBound(cfg.Listen.IpPort) {
+		return nil, fmt.Errorf("NewWebServer error: could not start because port already in-use on '%s'", cfg.Listen.IpPort)
+	}
+
+	s := &WebServer{
+		Cfg:         cfg,
+		ServerReady: make(chan bool),
+		Done:        make(chan bool),
+		requestStop: make(chan bool),
+	}
+
+	s.tts = NewCustomHttpServer(s.Cfg.Listen.IpPort, mux, s.Cfg.ReadTimeout)
+	//s.tts = NewCustomHttpServer(s.Cfg.Addr, http.DefaultServeMux) // supply debug/pprof diagnostics
+
+	return s, nil
+}
+
+func (s *WebServer) Start(webName string) {
+	s.Name = webName
+	if s.started {
+		return
+	}
+	s.started = true
+	po("WebServer::Start('%s') begun, for s = %p.  Listen: %v", webName, s, s.Cfg.Listen.IpPort)
+
+	go func() {
+		err := s.tts.ListenAndServe()
+		if nil != err {
+			po("WebServer::Start('%s') done with s.tts.ListenAndServer(); err = '%s'.\n", webName, err)
+			//log.Println(err) // accept tcp 127.0.0.1:3000: use of closed network connection
+		}
+		s.stopped = true
+		close(s.Done)
+		po("WebServer::Start() inner goroutine done, for s = %p.\n", s)
+	}()
+
+	WaitUntilServerUp(s.Cfg.Listen.IpPort)
+	close(s.ServerReady)
+}
+
+func (s *WebServer) Stop() {
+	if s.stopped || s.IsStopRequested() {
+		// not stopping races here, just preventing
+		// panic on two serial Stops() under web_test.go failure situation.
+		return
+	}
+	VPrintf("in WebServer::Stop() about to request stop ... s = %p\n", s)
+	weClosed := s.RequestStop()
+	if weClosed {
+		s.tts.Close() // without weClosed check, hang here because tts is already down and gone.
+	}
+	VPrintf("in WebServer::Stop() after s.tts.Close() ... s = %p\n", s)
+	<-s.Done
+	VPrintf("in WebServer::Stop() after <-s.Done(): s.Addr = '%s' ... s = %p\n", s.Cfg.Listen.IpPort, s)
+
+	WaitUntilServerDown(s.Cfg.Listen.IpPort)
+}
+
+func (s *WebServer) IsStopRequested() bool {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	select {
+	case <-s.requestStop:
+		return true
+	default:
+		return false
+	}
+}
+
+// RequestStop makes sure we only close
+// the s.requestStop channel once. Returns
+// true iff we closed s.requestStop on this call.
+func (s *WebServer) RequestStop() bool {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	select {
+	case <-s.requestStop:
+		return false
+	default:
+		close(s.requestStop)
+		return true
+	}
+}
+
+func WaitUntilServerUp(addr string) {
+	attempt := 1
+	for {
+		if PortIsBound(addr) {
+			return
+		}
+		time.Sleep(500 * time.Millisecond)
+		attempt++
+		if attempt > 40 {
+			panic(fmt.Sprintf("could not connect to server at '%s' after 40 tries of 500msec", addr))
+		}
+	}
+}
+
+func WaitUntilServerDown(addr string) {
+	attempt := 1
+	for {
+		if !PortIsBound(addr) {
+			return
+		}
+		//fmt.Printf("WaitUntilServerUp: on attempt %d, sleep then try again\n", attempt)
+		time.Sleep(500 * time.Millisecond)
+		attempt++
+		if attempt > 40 {
+			panic(fmt.Sprintf("could always connect to server at '%s' after 40 tries of 500msec", addr))
+		}
+	}
+}
+
+func PortIsBound(addr string) bool {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return false
+	}
+	VPrintf("\n\n PortIsBound opened as client %v -> %v\n\n", conn.LocalAddr(), conn.RemoteAddr())
+	conn.Close()
+	return true
+}
+
+func FetchUrl(url string) ([]byte, error) {
+	response, err := http.Get(url)
+
+	defer func() {
+		if response != nil && response.Body != nil {
+			response.Body.Close()
+		}
+	}()
+	if err != nil {
+		return []byte{}, err
+	} else {
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return []byte{}, err
+		}
+		return contents, nil
+	}
+}
+
+/*
+// mock for http.ResponseWriter
+
+type MockResponseWriter struct{}
+
+// Header returns the header map that will be sent by WriteHeader.
+// Changing the header after a call to WriteHeader (or Write) has
+// no effect.
+func (m *MockResponseWriter) Header() http.Header {
+	return make(http.Header)
+}
+
+// Write writes the data to the connection as part of an HTTP reply.
+// If WriteHeader has not yet been called, Write calls WriteHeader(http.StatusOK)
+// before writing the data.  If the Header does not contain a
+// Content-Type line, Write adds a Content-Type set to the result of passing
+// the initial 512 bytes of written data to DetectContentType.
+func (m *MockResponseWriter) Write(p []byte) (int, error) {
+	return len(p), nil
+}
+
+// WriteHeader sends an HTTP response header with status code.
+// If WriteHeader is not called explicitly, the first call to Write
+// will trigger an implicit WriteHeader(http.StatusOK).
+// Thus explicit calls to WriteHeader are mainly used to
+// send error codes.
+func (m *MockResponseWriter) WriteHeader(status int) {
+
+}
+*/
